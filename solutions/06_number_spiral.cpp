9aaa6e2ede25f8cb1e3aa0113170cf7b821f9aa8
Himanshu Dwivedi <77741796+HimanshuVDwivedi@users.noreply.github.com>
2026-05-27 16:22:24 +0530

    Update 06_number_spiral.cpp

--- solutions/06_number_spiral.cpp
+++ solutions/06_number_spiral.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long y, x;
+        cin >> y >> x;
+        long long layer = max(y, x);
+        long long val;
+        if (layer % 2 == 0) {
+            if (y < x)
+                val = layer * layer - (layer - 1) + (layer - y);
+            else
+                val = (layer - 1) * (layer - 1) + x;
+        } else {
+            if (x < y)
+                val = layer * layer - (layer - 1) + (layer - x);
+            else
+                val = (layer - 1) * (layer - 1) + y;
+        }
+        cout << val << "\n";
+    }
+    return 0;
+}
