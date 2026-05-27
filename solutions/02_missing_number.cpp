9760c0a852be3c81254b945a8013dec43afe0aca
Himanshu Dwivedi <77741796+HimanshuVDwivedi@users.noreply.github.com>
2026-05-27 16:24:33 +0530

    Update 02_missing_number.cpp

--- solutions/02_missing_number.cpp
+++ solutions/02_missing_number.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    long long n;
+    cin >> n;
+    long long total = n * (n + 1) / 2;
+    for (int i = 0; i < n - 1; i++) {
+        long long x;
+        cin >> x;
+        total -= x;
+    }
+    cout << total << endl;
+    return 0;
+}
