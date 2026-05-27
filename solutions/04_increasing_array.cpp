07ee41955236dc1e4b06510950d929b855321bcf
Himanshu Dwivedi <77741796+HimanshuVDwivedi@users.noreply.github.com>
2026-05-27 16:22:47 +0530

    Update 04_increasing_array.cpp

--- solutions/04_increasing_array.cpp
+++ solutions/04_increasing_array.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    long long moves = 0, prev = 0;
+    for (int i = 0; i < n; i++) {
+        long long x;
+        cin >> x;
+        if (x < prev)
+            moves += prev - x;
+        else
+            prev = x;
+    }
+    cout << moves << endl;
+    return 0;
+}
