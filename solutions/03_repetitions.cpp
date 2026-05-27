f7c2c301f4c3a31f935a082ae377f64f0b6d3338
Himanshu Dwivedi <77741796+HimanshuVDwivedi@users.noreply.github.com>
2026-05-27 16:24:20 +0530

    Update 03_repetitions.cpp

--- solutions/03_repetitions.cpp
+++ solutions/03_repetitions.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    string s;
+    cin >> s;
+    int maxLen = 1, curLen = 1;
+    for (int i = 1; i < (int)s.size(); i++) {
+        if (s[i] == s[i - 1])
+            curLen++;
+        else
+            curLen = 1;
+        maxLen = max(maxLen, curLen);
+    }
+    cout << maxLen << endl;
+    return 0;
+}
