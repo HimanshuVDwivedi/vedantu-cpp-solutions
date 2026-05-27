42839279c673e35785a847af3feca1033bd01ed5
Himanshu Dwivedi <77741796+HimanshuVDwivedi@users.noreply.github.com>
2026-05-27 16:22:12 +0530

    Update 07_two_knights.cpp

--- solutions/07_two_knights.cpp
+++ solutions/07_two_knights.cpp
@@ -1,1 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    for (long long k = 1; k <= n; k++) {
+        long long total = k * k;
+        long long allPairs = total * (total - 1) / 2;
+        long long attacks = 0;
+        if (k >= 3)
+            attacks = 4 * (k - 1) * (k - 2);
+        cout << allPairs - attacks << "\n";
+    }
+    return 0;
+}
